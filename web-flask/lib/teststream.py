3bcfbe300301800130801eb136553d96567b5a2d
Oscar Redondo <o.redondo91@gmail.com>
2020-03-23 18:11:42 +0100

    add version vagrant

--- web-flask/lib/teststream.py
+++
@@ -1,8 +0,0 @@
-import requests
-from contextlib import closing
-
-print(requests.get('http://127.0.0.1:5000/api/deploy').text)
-
-while True:
-    with closing(requests.get('http://127.0.0.1:5000/api/stream', stream=True)) as r:
-        print(r.text)
